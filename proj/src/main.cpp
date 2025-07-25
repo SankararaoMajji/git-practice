891e39d3cedb4b0bada8002de0844eb5f2ee024b
Sankar <sankar.m.india@gmail.com>
2025-07-25 12:15:04 +0530

    added one more ! mark

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	std::cout << "Hi World! My Frieds!" << std::endl;
-	std::cout << "Hi World! My Frieds!" << std::endl;
+	std::cout << "Hi World! My Frieds!!" << std::endl;
 	std::cout << "I'm Sankara Rao Majji" << std::endl;
 
 	return 0;
