ba8899f2c16eecdca5c3c0aafd43f3c98cf4d91a
SankararaoMajji <116250801+SankararaoMajji@users.noreply.github.com>
2025-07-25 12:13:49 +0530

    bifurcated wishing and the wisher

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -3,7 +3,8 @@
 int main()
 {
 	std::cout << "Hi World! My Frieds!" << std::endl;
-	std::cout << "Hi World! My Frieds! I'm Sankara Rao Majji" << std::endl;
+	std::cout << "Hi World! My Frieds!" << std::endl;
+	std::cout << "I'm Sankara Rao Majji" << std::endl;
 
 	return 0;
 }
